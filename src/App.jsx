b857f7a6ee9c701d12c5312b1719e1183cfd1163
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-06-20 12:17:58 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,20 @@ import './App.css'
 function App() {
   
   return (
-   <div className="w-full flex items-center justify-center flex-col bg-white p-4">
-    <div className='border-2 items-center bg-white dark:bg-gray-800 rounded-lg px-8 py-2 mx-8 ring shadow-xl ring-gray-900/5'>
-        <div className='w-full overflow-hidden mx-auto md:w-90 lg:w-90 xl:w-90 rounded-top'>
-            <img src={image} alt="" className='w-full  md:w-90 lg:w-90 xl:w-90 mx-auto'/>
+   <div className="w-full h-full mx-auto flex items-center justify-center flex-col bg-white shadow-lg px-4 py-6  rounded md:w-60">
+    <div className='max-sw-sm rounded overflow-hidden shadow-lg h-full'>
+        
+            <img src={image} alt="" className='w-full  px-6 py-4 md:w-90 lg:w-90  xl:w-90 mx-auto object-fill'/>
+        
+        <div className='container w-full px-8 py-6 mx-4 text-center'>
+            <h1 className='font-bold text-xl mb-2'>Christine Somebody</h1>
+            <p className='text-gray -700 text-base py-4'>Cybersecurity Expert</p>
         </div>
-        <div className='text-center px-4 mx-4'>
-            <h1 className='text-4xl p-8 font-bold shadow-2xl mb-6'>Christine Somebody</h1>
-            <p className='text-center py-1 shadow-2xs'>Cybersecurity Expert</p>
-        </div>
-        <div className='py-4 bg-amber-50 px-4'>
+        <div className=' bg-amber-50'>
        
         </div>
-        <div className='flex items-center justify-center flex-col my-2'>
-          <button class="bg-sky-500 hover:bg-sky-700 py-2 rounded-3xl px-4">More...</button>
+        <div className='px-6 pt-4 pb-2 text-center m-4'>
+          <button class="bg-sky-500 hover:bg-sky-700 py-2  px-4 rounded">More...</button>
         </div>
     </div>
         
