0b335bace6254f82822351434fd4609022382453
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-06-30 16:37:33 +0200

    project rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,27 @@ import './App.css'
 function App() {
   
   return (
-   <div className="w-full h-full mx-auto flex items-center justify-center flex-col bg-white shadow-lg px-4 py-6  rounded md:w-60">
-    <div className='max-sw-sm rounded overflow-hidden shadow-lg h-full'>
-        
-            <img src={image} alt="" className='w-full  px-6 py-4 md:w-90 lg:w-90  xl:w-90 mx-auto object-fill'/>
-        
-        <div className='container w-full px-8 py-6 mx-4 text-center'>
-            <h1 className='font-bold text-xl mb-2'>Christine Somebody</h1>
-            <p className='text-gray -700 text-base py-4'>Cybersecurity Expert</p>
-        </div>
-        <div className=' bg-amber-50'>
-       
-        </div>
-        <div className='px-6 pt-4 pb-2 text-center m-4'>
-          <button class="bg-sky-500 hover:bg-sky-700 py-2  px-4 rounded">More...</button>
-        </div>
+   <div className=" md:w-80 mx-auto bg-white p-10">
+
+    <img src={image} alt='photo' className='pb-20'/>
+
+    <div className='flex justify-center items-center flex-col p-10 '>
+      <h1 className='text-3xl text-bold'>Cristine Something</h1>
+      <p>CyberSecurity Expert</p>
+      <div className=' flex justify-center items-center gap-2 py-4 hidden'>
+          <div className='bg-red-500 w-20 h-20 rounded-full border-2'>
+              <p>Email: Chistinesome@nuttycode.co</p>
+          </div>
+          <div className='bg-red-500 w-20 h-20 rounded-full border-2'>
+            Discord: Jsomething
+          </div>
+          <div className='bg-red-500 w-20 h-20 rounded-full border-2'>
+            Twitter: RozeQuarts
+          </div>
+      </div>
+      <button className='py-6 px-8 bg-red m-8 w-50'>More</button>
     </div>
+   
         
         
   </div>
