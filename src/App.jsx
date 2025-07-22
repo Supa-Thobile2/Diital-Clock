744b894bd3b59ebbe700b8d910276448c7d4f1b0
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-07-22 17:27:46 +0200

    added updates

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,26 @@ function App() {
     setIsVisible(!isVisible)
   }
   return (
-   <div className=" md:w-80 mx-auto bg-white p-10">
+   <div className=" w-70 mx-auto bg-white p-2">
 
-    <img src={image} alt='photo' className='pb-20'/>
+    <img src={image} alt='photo' className='object-contain'/>
 
-    <div className='flex justify-center items-center flex-col p-10 '>
-      <h1 className='text-3xl text-bold'>Cristine Something</h1>
-      <p>CyberSecurity Expert</p>
-      {isVisible && <div className=' flex justify-center items-center gap-2 py-4'>
-          <div className='bg-red-500 w-20 h-20 rounded-full border-2'>
-              <p>Email: Chistinesome@nuttycode.co</p>
+    <div className='flex justify-center items-center flex-col m-2'>
+      <h1 className='text-2xl text-bold pt-2'>Cristine Something</h1>
+      <p className='pb-2 text-base'>Cyber Security Expert</p>
+      {isVisible && <div className=' flex justify-center items-center gap-2 my-4'>
+          <div className='bg-red-500 w-10 h-10 rounded-full border-2 flex justify-center items-center'>
+              icon
           </div>
-          <div className='bg-red-500 w-20 h-20 rounded-full border-2'>
-            Discord: Jsomething
+          <div className='bg-red-500 w-10 h-10 rounded-full border-2 flex justify-center items-center'>
+            icon
           </div>
-          <div className='bg-red-500 w-20 h-20 rounded-full border-2'>
-            Twitter: RozeQuarts
+          <div className='bg-red-500 w-10 h-10 rounded-full border-2 flex justify-center items-center'>
+            icon
           </div>
       </div>}
       
-      <button className='py-6 px-8 bg-red m-8 w-50' onClick={toggleVisibility}>More</button>
+      <button className='bg-blue-300 py-6 px-8 bg-red m-8 w-50 rounded-3xl pb-4' onClick={toggleVisibility}>More</button>
     </div>
    
         
